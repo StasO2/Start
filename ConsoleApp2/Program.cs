8bbdf9cfcd90ed3c7d2b1dae6d06db94867ef860
talko <talko@DESKTOP-AG0ICEO>
2023-09-15 10:59:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Создание пространства имён(Как я понял это что-то вроде сборки классов)
+namespace ConsoleApp2
+{
+    //Создание класса
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console - Представляет стандартный пути ввода и вывода для консольных приложений
+            // Write - выводит строку в стандартный канал вывода консоли
+            Console.WriteLine("Hello, World!");
+            Console.Write("Я начал учиться!!!");
+            Console.WriteLine(" YPA!!!");
+        }
+    }
+}
